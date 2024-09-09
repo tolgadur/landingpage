c109feba8dcd4ced5a51826a0beb46da02089414
Tolga Dur <tolgadur@Tolgas-MacBook-Air.local>
2024-09-09 22:39:32 +0100

    remove dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { Header } from "@/components/common/header";
 import { Footer } from "@/components/common/footer";
-import { ThemeProvider } from "@/components/common/theme-provider";
 
 export const metadata: Metadata = {
   title: "Landingpage",
@@ -19,22 +18,13 @@ export default function RootLayout({
     <html lang="en" className="antialiased">
       <Analytics />
       <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className={`flex min-h-screen flex-col`}>
-            <Header />
-            <div className="flex flex-1 justify-center w-full">
-              <div className="flex w-full max-w-[1280px] h-full">
-                {children}
-              </div>
-            </div>
-            <Footer />
-          </main>
-        </ThemeProvider>
+        <main className={`flex min-h-screen flex-col`}>
+          <Header />
+          <div className="flex flex-1 justify-center w-full">
+            <div className="flex w-full max-w-[1280px] h-full">{children}</div>
+          </div>
+          <Footer />
+        </main>
       </body>
     </html>
   );
